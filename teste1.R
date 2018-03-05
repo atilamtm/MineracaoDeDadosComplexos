53fc000811132a6a3207c413452effe7f957ace9
Atila Moretto <atilam@motorola.com>
2018-03-04 21:50:54 -0300

    First homework code as received

---
+++ teste1.R
@@ -0,0 +1,51 @@
+########################################
+# Teste 1 - INF-0612          
+# Nome: 
+########################################
+
+## Criamos um vetor "x" com 17 posicoes, com numeros de 4 ate 20
+x <- 4:20
+
+## Exibimos os elementos de "x" entre as posicoes 5 e 8 (neste caso, os
+## elementos 8, 9, 10 e 11)
+x[5:8]
+
+## Exibimos os elementos de "x" nas posicoes 11, 10 e 9, respectivamente (neste
+## caso, os elementos 14, 13 e 12)
+x[11:9]
+
+## Salve na variavel "y", o vetor "x" invertido, ou seja, o primeiro
+## elemento de "x" deve ser o ultimo elemento de "y", o segundo
+## elemento de "x" deve ser o penultimo elemento de "y", e assim por
+## diante.  
+y <-
+
+## Imprima os elementos impares de "y"
+y[]
+
+## Imprima os elementos de "x" estritamente maiores que 8
+x[]
+
+## Criamos um vetor de nomes chamado "names"
+names <- c("Joao", "Laura", "Carlos", "Bruna", "Pedro", "Joao", "Maria")
+
+## Criamos um vetor de idades chamado "ages"
+ages <- c(80, 19, 32, 23, 10, 44, 40)
+
+## Criamos um vetor booleano chamado "student"
+student <- c(F, T, F, T, F, T, T)
+
+## Imprima as posicoes em "names" tal que estas posicoes sejam TRUE em
+## "student".  Por exemplo, como a posicao 1 em "student" eh FALSE,
+## "Joao", que esta na posicao 1 do vetor "names" nao deve ser
+## impressa. Ja a posicao 2 de "student" eh TRUE, entao "Laura", que
+## esta na posicao 2 do vetor "names", deve ser impressa
+names[]
+
+## Crie (e exiba) um data frame chamado "dados" tal que:
+##  -a primeira coluna, chamada "nome", seja o vetor "names" criado acima;
+##  -a segunda coluna, chamada "idade", seja o vetor "ages" criado acima;
+##  -a terceira coluna, chamada "matricula", seja o vetor "student" criado acima.
+dados <- 
+
+dados
