c55ab30857dde7e50bc9837c6e78194a2d6eeb0e
Atila Moretto <atilam@motorola.com>
2018-03-24 09:53:25 -0300

    adding exercise 4

---
+++ teste4.R
@@ -0,0 +1,21 @@
+########################################
+# Teste 4 - INF-0612          
+# Nome(s): 
+########################################
+
+names <- c("Horario", "Temperatura", "Vento", "Umidade", "Sensacao")
+cepagri <- read.csv("cepagri.csv", header = FALSE, sep = ";", col.names = names)
+
+library(ggplot2)
+
+## 1 - Umidade Relativa do Ar
+
+
+## 2 - Sensação Térmica da Segunda Quinzena do Mês
+
+
+## 3 - Temperatura dos Últimos Sete Dias do Mês
+
+
+## 4 - Ventos do Primeiro Dia do Mês
+
